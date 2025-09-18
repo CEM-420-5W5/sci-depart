c412da78b04108e3ae8a7567eb6c04668feaf26f
PierreOlivierBrillant <pierreolivierbrillant@gmail.com>
2025-09-18 16:04:40 -0400

    Fix Json

--- Models/Models/PlayableCard.cs
+++ Models/Models/PlayableCard.cs
@@ -1,4 +1,5 @@
-﻿using Models.Interfaces;
+﻿using System.Text.Json.Serialization;
+using Models.Interfaces;
 
 namespace Models.Models
 {
@@ -22,18 +23,22 @@ namespace Models.Models
 
         // Si la carte à jouer est dans la pioche du joueur
         public int? CardsPileOwnerId { get; set; }
+        [JsonIgnore]
         public virtual MatchPlayerData? CardsPileOwner { get; set; }
 
         // Si la carte à jouer est dans la main du joueur
         public int? HandOwnerId { get; set; }
+        [JsonIgnore]
         public virtual MatchPlayerData? HandOwner { get; set; }
 
         // Si la carte à jouer est sur le champ de battaille du joueur
         public int? BattleFieldOwnerId { get; set; }
+        [JsonIgnore]
         public virtual MatchPlayerData? BattleFieldOwner { get; set; }
 
         // Si la carte à jouer est au cimetière du joueur (elle a été tuée)
         public int? GraveyardOwnerId { get; set; }
+	    [JsonIgnore]
         public virtual MatchPlayerData? GraveyardOwner { get; set; }
     }
 }
