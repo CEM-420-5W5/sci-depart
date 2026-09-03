374e0f3cccaa74fa402a2f8e32c085df49d30ed9
Mathieu Briau <mathieu.briau@gmail.com>
2026-09-03 09:05:59 -0400

    AccountController

---
+++ WebApi/Controllers/AccountController.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Models.Data;
+using Models.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using WebApi.Services;
+using WebAPI.DTOs;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private PlayersService _playersService;
+        private ApplicationDbContext _dbContext;
+
+        public AccountController(   UserManager<IdentityUser> userManager,
+                                    SignInManager<IdentityUser> signInManager,
+                                    PlayersService playersService,
+                                    ApplicationDbContext dbContext)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _playersService = playersService;
+            _dbContext = dbContext;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Login(LoginDTO loginDTO)
+        {
+            var result = await _signInManager.PasswordSignInAsync(loginDTO.Username, loginDTO.Password, true, lockoutOnFailure: false);
+
+            if (result.Succeeded)
+            {
+                Claim nameIdentifierClaim = User.Claims.First(x => x.Type == ClaimTypes.NameIdentifier);
+
+                // Note: On ajoute simplement le NameIdentifier dans les claims. Il n'y aura pas de rôle pour les utilisateurs du WebAPI.
+                List<Claim> authClaims = new List<Claim>();
+                authClaims.Add(nameIdentifierClaim);
+
+                SymmetricSecurityKey signingKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("C'est tellement la meilleure cle qui a jamais ete cree dans l'histoire de l'humanite (doit etre longue)"));
+
+                string issuer = this.Request.Scheme + "://" + this.Request.Host;
+                
+                DateTime expirationTime = DateTime.Now.AddMinutes(30);
+
+                JwtSecurityToken token = new JwtSecurityToken(
+                    issuer: issuer,
+                    audience: null,
+                    claims: authClaims,
+                    expires: expirationTime,
+                    signingCredentials: new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256Signature)
+                );
+
+                string tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+
+                Player player = _playersService.GetPlayerFromUserId(nameIdentifierClaim.Value);
+
+                // On ne veut JAMAIS retouner une string directement lorsque l'on utilise Angular.
+                // Angular assume que l'on retourne un objet et donne une erreur lorsque le résultat obtenu est une simple string!
+                return Ok(new LoginSuccessDTO(tokenString, player.Id, player.Name));
+            }
+
+            return NotFound(new { Error = "L'utilisateur est introuvable ou le mot de passe ne concorde pas" });
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult> Register(RegisterDTO registerDTO)
+        {
+
+            if (registerDTO.Password != registerDTO.PasswordConfirm)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { Error = "Le mot de passe et la confirmation ne sont pas identique" });
+            }
+
+            IdentityUser user = new IdentityUser()
+            {
+                UserName = registerDTO.Username,
+                Email = registerDTO.Email
+            };
+            IdentityResult identityResult = await _userManager.CreateAsync(user, registerDTO.Password);
+
+            if (!identityResult.Succeeded)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { Error = identityResult.Errors });
+            }
+
+            _playersService.CreatePlayer(user);
+
+            return await Login(new LoginDTO() { Username = registerDTO.Username, Password = registerDTO.Password});
+        }
+
+        [Authorize]
+        [HttpGet]
+        public ActionResult<string[]> PrivateData()
+        {
+            return new string[] { "figue", "banane", "noix" };
+        }
+
+        [HttpGet]
+        public ActionResult<string[]> PublicData()
+        {
+            return new string[] { "public", "publique" };
+        }
+    }
+}
---
+++ WebApi/DTOs/LoginSuccessDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.DTOs
+{
+    public class LoginSuccessDTO
+    {
+        public LoginSuccessDTO(string token, int playerId, string playerName)
+        {
+            Token = token;
+            PlayerId = playerId;
+            PlayerName = playerName;
+        }
+
+        [Required]
+        public string Token { get; set; }
+        public int PlayerId { get; set; }
+        public string PlayerName { get; set; }
+    }
+}
