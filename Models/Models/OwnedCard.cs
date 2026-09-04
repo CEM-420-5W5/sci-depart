358900003264e605760f977703a44d8a79a653f3
Mathieu Briau <mathieu.briau@cegepmontpetit.ca>
2026-09-04 14:22:14 -0400

    OwnedCard et Stub

---
+++ Models/Models/OwnedCard.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+using Models.Interfaces;
+
+namespace Models.Models
+{
+    public class OwnedCard:IModel
+	{
+		public OwnedCard() { }
+
+        public int Id { get; set; }
+        public virtual Card Card { get; set; }
+        [JsonIgnore]
+        public virtual Player Player { get; set; }
+    }
+}
+
--- WebApi/Services/CardsService.cs
+++ WebApi/Services/CardsService.cs
@@ -12,12 +12,27 @@ namespace WebApi.Services
             _dbContext = dbContext;
         }
 
-        public IEnumerable<Card> GetPlayersCards(string userId)
+        public IEnumerable<OwnedCard> GetPlayersCards(string userId)
         {
-            // Stub: Pour l'intant, le stub retourne simplement 1 copie des 8 premières cartes
+            // Stub: Pour l'intant, le stub retourne simplement 2 copies des 8 premières cartes
             // L'implémentation réelle devra utiliser un service et retourner les cartes qu'un joueur possède
-            // L'implémentation est la responsabilité de la personne en charge de la partie [Cartes]
-            return _dbContext.Cards.Take(8).ToList();
+            // L'implémentation est la responsabilité de la personne en charge des [cartes]
+            var cards = _dbContext.Cards.Take(8);
+            cards = cards.Concat(cards);
+
+            // Pour l'instant on utilise l'index comme Id pour que chaque OwnedCard est un Id différent
+            // Le champ player n'est pas utile pour l'instant, mais il devra être bien utilisé dans la vrai implémentation
+            var ownedCards = cards.Select((card, index) => new OwnedCard() { Card = card, Id = index + 1 }).ToList();
+            
+            return ownedCards;
+        }
+
+        public IEnumerable<OwnedCard> GetPlayersCardsForMatch(string userId)
+        {
+            // Stub: Pour l'intant, le stub retourne simplement les cartes du joueur. Mais ça va être utile pour créer un Match avec les bonnes cartes.
+            // L'implémentation réelle devra retourner les cartes du deck courrant
+            // L'implémentation est la responsabilité de la personne en charge des [decks]
+            return GetPlayersCards(userId);
         }
 
         public IEnumerable<Card> GetAllCards()
